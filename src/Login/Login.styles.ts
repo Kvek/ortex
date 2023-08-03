7ec48146ed246cdc4c90c10bd12763a40cb3440c
Vivek <vkavek.dev@pm.me>
2023-08-03 15:27:20 +0100

    updated login form styled

--- src/Login/Login.styles.ts
+++ src/Login/Login.styles.ts
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import styled from "styled-components/macro";
 import { device } from "../constants";
 
 type ButtonProps = {
@@ -7,19 +7,26 @@ type ButtonProps = {
 
 export const LoginContainer = styled.div`
   display: flex;
-  width: 100%;
-  height: 100%;
   align-items: center;
   justify-content: center;
   color: #fff;
+  background-color: rgb(0, 0, 0, 0.95);
+  flex: 3;
+  width: 100%;
+
+  @media ${device.tablet} {
+    height: 100%;
+    flex: 1;
+  }
 `;
 
 export const LoginFormWrapper = styled.div`
-  width: 450px;
+  width: 100%;
+  height: 100%;
 
-  @media ${device.mobile} {
-    width: 100%;
-    height: 420px;
+  @media ${device.tablet} {
+    height: 450px;
+    width: 400px;
   }
 `;
 
@@ -27,22 +34,31 @@ export const LoginFormContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  border: 1px solid #ffffff24;
   padding: 1em;
   border-radius: 10px;
   box-sizing: border-box;
+  height: 100%;
+  box-shadow: none;
+
+  @media ${device.tablet} {
+    box-shadow: 0 0 1px 1px #ffffff24;
+  }
 `;
 
 export const LogoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 60px;
-  min-height: 60px;
-  border-radius: 10px;
-  padding: 1em 0;
-  color: rgb(136, 136, 136, 0.6);
+  display: none;
+
+  @media ${device.tablet} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 60px;
+    min-height: 60px;
+    border-radius: 10px;
+    padding: 1em 0;
+    color: rgb(136, 136, 136, 0.6);
+  }
 `;
 
 export const InputContainer = styled.div`
@@ -50,6 +66,8 @@ export const InputContainer = styled.div`
   flex-direction: column;
   padding: 1em 0;
   overflow: hidden;
+  flex: 2;
+  justify-content: center;
 
   div {
     transition: max-height 0.15s linear;
@@ -80,7 +98,7 @@ export const Button = styled.button<ButtonProps>`
   background: none;
   color: rgba(255, 255, 255, 1);
   min-width: 150px;
-  max-width: 300px;
+  max-width: 200px;
   width: 100%;
   padding: 10px;
   margin: 10px;
@@ -146,8 +164,4 @@ export const Button = styled.button<ButtonProps>`
       `;
     }
   }}
-
-  @media ${device.mobile} {
-    max-width: 200px;
-  }
 `;
